3ab2c70940f915eb46f451bf0cbdfd0acaf7206d
Chaitanya Sai Kumar Bhogadula <81066341+ChaitanyaBhogadula@users.noreply.github.com>
2022-12-04 23:53:53 +0530

    Update Farmer.sql

--- Farmer.sql
+++ Farmer.sql
@@ -3,24 +3,24 @@
   /*Farmer table creation*/
 
 create table farmer(
-  name varchar2(30),
-  email varchar2(40),
-  phone varchar2(10),
-  addr varchar2(100),
-  pwd varchar2(20)
-);
+        name varchar2(30),
+        email varchar2(40),
+        phone varchar2(10),
+        addr varchar2(100),
+        pwd varchar2(20)
+    );
 
   /*Stored Procedure to insert a record into database table*/
 
 create or replace procedure insert_farmer(
-  name varchar2,
-  email varchar2,
-  phone varchar2,
-  addr varchar2,
-  pwd varchar2
+    name varchar2,
+    email varchar2,
+    phone varchar2,
+    addr varchar2,
+    pwd varchar2
 ) is
 
-Begin
+begin
 Insert into farmer values(name,email,phone,addr,pwd);
 Commit;
 End;
@@ -29,16 +29,16 @@ End;
 /*Function to verify Login :*/
 
 create or replace function verifylogin(
-  mail varchar2,
-  pw varchar2
+    mail varchar2,
+    pw varchar2
 ) return number is r number;
 
 begin
 select count(*) into r from farmer where email=mail and pwd=pw;
 if r>0 then
-return 1;
+    return 1;
 else
-return 0;
+    return 0;
 end if;
-end;
+END;
 /
